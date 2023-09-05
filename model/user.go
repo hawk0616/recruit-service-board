b530502e0d7f0d1018973a9fbedbc87c42181b83
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-09-05 23:56:06 +0900

    gitignore追加

--- model/user.go
+++ model/user.go
@@ -4,6 +4,7 @@ import "time"
 
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name"`
 	Email     string    `json:"email" gorm:"unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
@@ -12,5 +13,6 @@ type User struct {
 
 type UserResponse struct {
 	ID    uint   `json:"id" gorm:"primaryKey"`
+	Name  string  `json:"name"`
 	Email string `json:"email" gorm:"unique"`
 }
